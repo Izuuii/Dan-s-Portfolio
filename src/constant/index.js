543cd9a163c891ce0ae18e08921d41eebee74d7f
Izuuii <denverdeleon22@gmail.com>
2025-06-25 14:29:58 +0800

    fix: added missing nodeJS.png or fixed path

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,51 +1,51 @@
-import bootstrap from "../assets/images/bootstrap-original.png";
-import css3 from "../assets/images/css3-original.png";
-import figma from "../assets/images/figma-original.png";
-import git from "../assets/images/git-plain.png";
-import html5 from "../assets/images/html5-original.png";
-import js from "../assets/images/javascript-original.png";
-import react from "../assets/images/react-original.png";
-import tailwind from "../assets/images/tailwindcss-plain.png";
-import vscode from "../assets/images/vscode-original.png";
-import canva from "../assets/images/canva.png";
-import expressjs from "../assets/images/express-js.png"
-import firebase from "../assets/images/firebase.png"
-import mongoDB from "../assets/images/mongoDB.png"
-import nodeJS from "../assets/images/nodeJS.png"
+// import bootstrap from "../assets/images/bootstrap-original.png";
+// import css3 from "../assets/images/css3-original.png";
+// import figma from "../assets/images/figma-original.png";
+// import git from "../assets/images/git-plain.png";
+// import html5 from "../assets/images/html5-original.png";
+// import js from "../assets/images/javascript-original.png";
+// import react from "../assets/images/react-original.png";
+// import tailwind from "../assets/images/tailwindcss-plain.png";
+// import vscode from "../assets/images/vscode-original.png";
+// import canva from "../assets/images/canva.png";
+// import expressjs from "../assets/images/express-js.png"
+// import firebase from "../assets/images/firebase.png"
+// import mongoDB from "../assets/images/mongoDB.png"
+// import NodeJS from "../assets/images/nodeJS.png";
 
-export const techIcons = [
-  { imgUrl: bootstrap },
-  { imgUrl: css3 },
-  { imgUrl: figma },
-  { imgUrl: git },
-  { imgUrl: html5 },
-  { imgUrl: js },
-  { imgUrl: react },
-  { imgUrl: tailwind },
-  { imgUrl: vscode },
-  { imgUrl: canva },
-  { imgUrl: expressjs },
-  { imgUrl: firebase },
-  { imgUrl: mongoDB },
-  { imgUrl: nodeJS },
+// export const techIcons = [
+//   { imgUrl: bootstrap },
+//   { imgUrl: css3 },
+//   { imgUrl: figma },
+//   { imgUrl: git },
+//   { imgUrl: html5 },
+//   { imgUrl: js },
+//   { imgUrl: react },
+//   { imgUrl: tailwind },
+//   { imgUrl: vscode },
+//   { imgUrl: canva },
+//   { imgUrl: expressjs },
+//   { imgUrl: firebase },
+//   { imgUrl: mongoDB },
 
 
-  { imgUrl: bootstrap },
-  { imgUrl: css3 },
-  { imgUrl: figma },
-  { imgUrl: git },
-  { imgUrl: html5 },
-  { imgUrl: js },
-  { imgUrl: react },
-  { imgUrl: tailwind },
-  { imgUrl: vscode },
-  { imgUrl: canva },
-  { imgUrl: expressjs },
-  { imgUrl: firebase },
-  { imgUrl: mongoDB },
-  { imgUrl: nodeJS },
 
-];
+//   { imgUrl: bootstrap },
+//   { imgUrl: css3 },
+//   { imgUrl: figma },
+//   { imgUrl: git },
+//   { imgUrl: html5 },
+//   { imgUrl: js },
+//   { imgUrl: react },
+//   { imgUrl: tailwind },
+//   { imgUrl: vscode },
+//   { imgUrl: canva },
+//   { imgUrl: expressjs },
+//   { imgUrl: firebase },
+//   { imgUrl: mongoDB },
+
+
+// ];
 
 export const greetings = [
   
